4a55ac305391fe28ab4631c7f8f9fe5e0ce8f705
xiajing <hzxj0730@gmail.com>
2017-05-09 17:37:25 +0800

    页面模板

--- src/main/resources/templates/ItemDemoUI/src/components/RegisterPageTemplate.vue
+++ src/main/resources/templates/ItemDemoUI/src/components/RegisterPageTemplate.vue
@@ -11,8 +11,9 @@
         <label class="nameid">模板描述: </label> <input type="text" class="inputbox"  placeholder="121"
                            autocomplete="on" >
         <label class="nameid"> KEY: </label> <input type="text" class="inputbox"  placeholder="121"
-                      autocomplete="on" ><br/>
-        <label class="nameid">开发日期: </label> <input type="text" class="inputbox"  placeholder="121"
+                      autocomplete="on" >
+        <br/>
+        <label class="nameid">开发日期: </label><input type="text" class="inputbox"  placeholder="121"
                       autocomplete="on" >
         <label class="nameid">开发者: </label> </label><input type="text" class="inputbox"  placeholder="121"
                       autocomplete="on" >
@@ -22,21 +23,27 @@
     <div class="heads">
       页面流程结点
     </div>
-    <div class="drawb">
+    <div class="yellow">
       关联的流程结点：1，2
-      <div class="s">
-        <select name='make'>
-
-          <option>Make</option>
-          <option value='acura'>Acura</option>
-          <option value='chrysler' selected>Chrysler</option>
-          <option value='dodge'>Dodge</option>
-          <option value='ford'>Ford</option>
-          <option value='toyota'>Toyota</option>
-          <option value='derp'>A really long name for testing</option>
+        <select class="inputbox" >
+      <option>一级流程（多选）</option>
+      <option value='acura'>商品发布</option>
+      </select>
+      <select class="inputbox" >
+        <option>二级流程（多选）</option>
+        <option value='acura'>商品发布</option>
+      </select>
+      <select class="inputbox" >
+        <option>二级流程结点（多选）</option>
+        <option value='acura'>商品发布</option>
+      </select>
+        <select class="inputbox" >
+          <option>角色列表（多选）</option>
+          <option value='acura'>ID</option>
         </select>
-      </div>
-
+    </div>
+    <div class="heads">
+      关联的业务能力剖面
     </div>
 
   </div>
@@ -46,22 +53,25 @@
 <style>
 
   .regisitertemplate{
-    position: absolute;
-    top:200px;
+    position: relative;
+    top:20px;
     font-size: 14px;
     padding:0;
     margin:0;
+    height: 900px;
+    width: 90%;
   }
   .heads{
     text-align: left;
-    font: 15px/25px Arial, sans-serif;
+    font: 14px/30px Arial, sans-serif;
     background:#F2F2F2;
     position: relative;
     text-decoration: none;
     width:100%;
     height: 30px;
-    padding: 8px 20px;
-    margin-bottom: 20px;
+    padding:  0px 20px ;
+
+    margin-bottom: 10px;
   }
   .items{
     margin-bottom: 20px;
@@ -72,18 +82,25 @@
   }
   .itemsform{
     position: relative;
-    left:10px;
+    left:30px;
   }
   .nameid{
     display: inline-block;
     width:70px;
   }
   .inputbox{
-    border: 1px solid #f0f0f0;
+    border: 1px solid #999999;
     width:100px;
-    height: 30px;
-    margin-right: 30px;
+    height: 20px;
+    margin-right: 20px;
     margin-bottom: 10px;
+  }
+  .yellow{
+    position: relative;
+    left:30px;
+    padding-top:10px;
+    background: lightgoldenrodyellow;
+    margin-bottom: 20px;
   }
 
 
