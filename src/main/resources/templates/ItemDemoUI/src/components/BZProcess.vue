90719d40b6aa78a80d3f197c83161b8db17cafc9
Mingle <996030567@qq.com>
2017-05-09 21:20:41 +0800

    applicationRegister

--- src/main/resources/templates/ItemDemoUI/src/components/BZProcess.vue
+++ src/main/resources/templates/ItemDemoUI/src/components/BZProcess.vue
@@ -3,66 +3,59 @@
         <span class = "tabs-BZ-total">流程定制总数：12</span>
         <ul class="BZ-register">
           <li>分类</li>
-          <li class = "register"><button type = "submit" value="submit">注册流程</button> </li>
+          <li class = "register btn btn-primary">注册流程</li>
         </ul>
 
         <div class="BZ-classify">
           <img src="" alt="">
-          <a href=""><router-link to="">商品发布流程（2）</router-link></a>
-          <a href=""><router-link to="">配置前置条件</router-link></a>
-          <a href=""><router-link to="">查看前置条件</router-link></a>
-          <ul class="BZ-edit">
-            <li class = "BZ-num">1</li>
-            <li class = "process">此处放流程图</li>
-            <li class = "btn btn-primary">编辑</li>
-            <li class = "btn btn-primary">查看</li>
-            <li class = "btn btn-primary">删除</li>
-            <span></span>
-
-            <li class = "BZ-num">2</li>
-            <li class = "process">此处放流程图</li>
-            <li class = "btn btn-primary">编辑</li>
-            <li class = "btn btn-primary">查看</li>
-            <li class = "btn btn-primary">删除</li>
-            <span></span>
-          </ul>
-          
-          <!-- <img src="../assets/img/logo.jpg" alt=""> -->
-          <img src="" alt="">
-          <a href=""><router-link to="">商品发布流程（2）</router-link></a>
-          <a href=""><router-link to="">配置前置条件</router-link></a>
-          <a href=""><router-link to="">查看前置条件</router-link></a>
-          <ul class="BZ-edit">
-            <li class = "BZ-num">1</li>
-            <li class = "process">此处放流程图</li>
-            <li class = "btn btn-primary">编辑</li>
-            <li class = "btn btn-primary">查看</li>
-            <li class = "btn btn-primary">删除</li>
-            <span></span>
-
-            <li class = "BZ-num">2</li>
-            <li class = "process">此处放流程图</li>
-            <li class = "btn btn-primary">编辑</li>
-            <li class = "btn btn-primary">查看</li>
-            <li class = "btn btn-primary">删除</li>
-            <span></span>
-          </ul>
-
-
-          <!-- <div class="container">
-            <div class="row">
-              <div class="col-sm-2">1.</div>
-              <div class="col-sm-4">流程图</div>
-              <div class="col-sm-2"><button>编辑</button></div>
-              <div class="col-sm-2"><button>查看</button></div>
-              <div class="col-sm-2"><button>删除</button></div>
-            </div>
-          </div> -->
+          <a href="" ><router-link to="" class = "L1-name">交易下单流程（2）</router-link></a>
+          <a href="" ><router-link to="" class = "L1-edit">配置前置条件</router-link></a>
+          <a href="" ><router-link to="" class = "L1-edit">查看前置条件</router-link></a>
+          <div class = "BZ-table">
+            <ul class="BZ-edit">
+              <li class = "BZ-num">1</li>
+              <li class = "process">此处放流程图</li>
+              <li class = "btn btn-primary">编辑</li>
+              <li class = "btn btn-primary">查看</li>
+              <li class = "btn btn-primary">删除</li>
+            </ul>
+            <hr class = "hr hr-BZProcess">
+            <ul class="BZ-edit">
+                <li class = "BZ-num">2</li>
+                <li class = "process">此处放流程图</li>
+                <li class = "btn btn-primary">编辑</li>
+                <li class = "btn btn-primary">查看</li>
+                <li class = "btn btn-primary">删除</li>
+            </ul> 
+          </div>  
         </div>
+
+        <div class="BZ-classify">
+          <img src="" alt="">
+          <a href="" ><router-link to="" class = "L1-name">交易下单流程（2）</router-link></a>
+          <a href="" ><router-link to="" class = "L1-edit">配置前置条件</router-link></a>
+          <a href="" ><router-link to="" class = "L1-edit">查看前置条件</router-link></a>
+          <div class = "BZ-table">
+            <ul class="BZ-edit">
+              <li class = "BZ-num">1</li>
+              <li class = "process">此处放流程图</li>
+              <li class = "btn btn-primary">编辑</li>
+              <li class = "btn btn-primary">查看</li>
+              <li class = "btn btn-primary">删除</li>
+            </ul>
+            <hr class = "hr hr-BZProcess">
+            <ul class="BZ-edit">
+                <li class = "BZ-num">2</li>
+                <li class = "process">此处放流程图</li>
+                <li class = "btn btn-primary">编辑</li>
+                <li class = "btn btn-primary">查看</li>
+                <li class = "btn btn-primary">删除</li>
+            </ul> 
+          </div>  
+        </div>
+  </div>
+
         
       </div>
 </template>
 
-<style>
-	.BZ-tabs{border:1px solid red;}
-</style>
--- src/main/resources/templates/ItemDemoUI/src/components/BZability.vue
+++ src/main/resources/templates/ItemDemoUI/src/components/BZability.vue
@@ -1,36 +1,4 @@
 <template>
-  <!-- <h1 class="text-center">bzability</h1> -->
-  <div class = "BZ-tabs">
-        <span class = "tabs-BZ-total">流程定制总数：12</span>
-        <ul class="BZ-register">
-          <li>业务能力</li>
-          <li class = "register"><button type = "submit" value="submit">注册业务能力</button> </li>
-        </ul>
+  <h1 class="text-center">bzability</h1>
 
-        <div class="BZ-classify">
-          <ul class="BZ-edit">
-            <li class = "BZ-num">描述标题1</li>
-            <li class = "process"></li>
-            <li><button type = "submit" value="submit">编辑</button></li>
-            <li><button type = "submit" value="submit">查看</button></li>
-            <li><button type = "submit" value="submit">删除</button></li>
-            <span></span>
-
-            <li class = "BZ-num">业务能力2</li>
-            <li class = "process"></li>
-            <li><button type = "submit" value="submit">编辑</button></li>
-            <li><button type = "submit" value="submit">查看</button></li>
-            <li><button type = "submit" value="submit">删除</button></li>
-            <span></span>
-
-            <li class = "BZ-num">业务能力3</li>
-            <li class = "process"></li>
-            <li><button type = "submit" value="submit">编辑</button></li>
-            <li><button type = "submit" value="submit">查看</button></li>
-            <li><button type = "submit" value="submit">删除</button></li>
-            <span></span>
-          </ul>
-        </div>
-        
-      </div>
 </template>
