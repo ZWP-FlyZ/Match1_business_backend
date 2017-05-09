64966665a7d7e52bb94525444b838d5fd360af18
wxfei <805915717@qq.com>
2017-05-09 14:16:25 +0800

    夏景 创建页面模板

--- src/main/resources/templates/ItemDemoUI/src/components/PageTemplate.vue
+++ src/main/resources/templates/ItemDemoUI/src/components/PageTemplate.vue
@@ -4,7 +4,7 @@
 
     <div class="template-head">
       <div class="head-left">页面模板</div>
-      <router-link to="/RegisterPageTemplate.vue"  class="head-creat">创建页面模板</router-link>
+      <router-link to="/registerPageTemplate"  class="head-creat">创建页面模板</router-link>
     </div>
 
     <div class="template-body">
