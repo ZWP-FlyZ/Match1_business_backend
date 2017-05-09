01aadb2813c19746b80b2708eee0e03b5b0d4be5
Mingle <996030567@qq.com>
2017-05-09 22:02:56 +0800

    BZ-Ability Ability

--- src/main/resources/templates/ItemDemoUI/src/components/Ability.vue
+++ src/main/resources/templates/ItemDemoUI/src/components/Ability.vue
@@ -1,36 +1,81 @@
 <template>
-  <!-- <h1 class="text-center">ability</h1> -->
-  <div class = "BZ-tabs">
-        <span class = "tabs-BZ-total">业务能力定制总数：12</span>
-        <ul class="BZ-register">
-          <li>能力</li>
-          <li class = "register"><button type = "submit" value="submit">创建能力</button> </li>
+  <!-- <h1 class="text-center">bzability</h1> -->
+  <div class = "BZability">
+    <div>能力定制总数：12</div>
+    <div class="BZability-register">
+      <ul>
+          <li class = "ability-reg">能力</li>
+          <li class = "btn btn-primary">创建能力</li>
+      </ul>
+    </div>
+    <div class = "BZability-list">
+      <div class = "BZability-list-one">
+        <ul>
+          <li class = "BZability-name">颜色</li>
+          <li class = "btn btn-primary">编辑</li>
+          <li class = "btn btn-primary">查看</li>
+          <li class = "btn btn-primary">删除</li>
         </ul>
-
-        <div class="BZ-classify">
-          <ul class="BZ-edit">
-            <li class = "BZ-num">颜色</li>
-            <li class = "process"></li>
-            <li><button type = "submit" value="submit">编辑</button></li>
-            <li><button type = "submit" value="submit">查看</button></li>
-            <li><button type = "submit" value="submit">删除</button></li>
-            <span></span>
-
-            <li class = "BZ-num">尺寸</li>
-            <li class = "process"></li>
-            <li><button type = "submit" value="submit">编辑</button></li>
-            <li><button type = "submit" value="submit">查看</button></li>
-            <li><button type = "submit" value="submit">删除</button></li>
-            <span></span>
-
-            <li class = "BZ-num">材质</li>
-            <li class = "process"></li>
-            <li><button type = "submit" value="submit">编辑</button></li>
-            <li><button type = "submit" value="submit">查看</button></li>
-            <li><button type = "submit" value="submit">删除</button></li>
-            <span></span>
-          </ul>
-        </div>
-        
       </div>
+      <div class = "BZability-list-one">
+        <ul>
+          <li class = "BZability-name">尺寸</li>
+          <li class = "btn btn-primary">编辑</li>
+          <li class = "btn btn-primary">查看</li>
+          <li class = "btn btn-primary">删除</li>
+        </ul>
+      </div>
+      <div class = "BZability-list-one">
+        <ul>
+          <li class = "BZability-name">无理由退货</li>
+          <li class = "btn btn-primary">编辑</li>
+          <li class = "btn btn-primary">查看</li>
+          <li class = "btn btn-primary">删除</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
 </template>
+<style>
+    .BZability-register{
+      background: #F2F2F2;
+      height: 45px;
+      margin-top: 30px;
+    }
+    .BZability-register li{
+      float: left;
+    }
+    .ability-reg{
+      margin-left: 10%;
+      margin-top:8px;
+    }
+    .BZability-register li.btn{
+      margin-left: 60%;
+      margin-top: 5px;
+    }
+
+    .BZability-list-one{
+      border: 1px solid #F2F2F2;
+      height: 50px;
+      margin-top: 13px;
+    }
+    .BZability-list-one li{
+      float: left;
+    }
+    .BZability-name{
+      margin-left: 3%;
+      margin-top: 8px;
+      margin-right: 50%;
+      width: 20%;
+    }
+    .BZability-list-one li.btn{
+      margin-top: 5px;
+      margin-right: 3%;
+    }
+    body{
+    font:15px/1.62 Helvetica,Arial,sans-serif;
+    background-color:#fff;
+  }
+
+  </style>
--- src/main/resources/templates/ItemDemoUI/src/components/BZability.vue
+++ src/main/resources/templates/ItemDemoUI/src/components/BZability.vue
@@ -1,4 +1,81 @@
 <template>
-  <h1 class="text-center">bzability</h1>
+  <!-- <h1 class="text-center">bzability</h1> -->
+  <div class = "BZability">
+    <div>模板定制总数：12</div>
+    <div class="BZability-register">
+      <ul>
+          <li class = "ability-reg">业务能力</li>
+          <li class = "btn btn-primary">注册业务能力</li>
+      </ul>
+    </div>
+    <div class = "BZability-list">
+      <div class = "BZability-list-one">
+        <ul>
+          <li class = "BZability-name">描述标题</li>
+          <li class = "btn btn-primary">编辑</li>
+          <li class = "btn btn-primary">查看</li>
+          <li class = "btn btn-primary">删除</li>
+        </ul>
+      </div>
+      <div class = "BZability-list-one">
+        <ul>
+          <li class = "BZability-name">业务能力1</li>
+          <li class = "btn btn-primary">编辑</li>
+          <li class = "btn btn-primary">查看</li>
+          <li class = "btn btn-primary">删除</li>
+        </ul>
+      </div>
+      <div class = "BZability-list-one">
+        <ul>
+          <li class = "BZability-name">业务能力2</li>
+          <li class = "btn btn-primary">编辑</li>
+          <li class = "btn btn-primary">查看</li>
+          <li class = "btn btn-primary">删除</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 
 </template>
+<style>
+    .BZability-register{
+      background: #F2F2F2;
+      height: 45px;
+      margin-top: 30px;
+    }
+    .BZability-register li{
+      float: left;
+    }
+    .ability-reg{
+      margin-left: 10%;
+      margin-top:8px;
+    }
+    .BZability-register li.btn{
+      margin-left: 60%;
+      margin-top: 5px;
+    }
+
+    .BZability-list-one{
+      border: 1px solid #F2F2F2;
+      height: 50px;
+      margin-top: 13px;
+    }
+    .BZability-list-one li{
+      float: left;
+    }
+    .BZability-name{
+      margin-left: 3%;
+      margin-top: 8px;
+      margin-right: 50%;
+      width: 20%;
+    }
+    .BZability-list-one li.btn{
+      margin-top: 5px;
+      margin-right: 3%;
+    }
+    body{
+    font:15px/1.62 Helvetica,Arial,sans-serif;
+    background-color:#fff;
+  }
+
+  </style>
