207051d2420617ffbf9c8bc63de8eb1025e51cbc
xiajing <hzxj0730@gmail.com>
2017-05-09 14:13:22 +0800

    页面模板

--- src/main/resources/templates/ItemDemoUI/src/components/RegisterPageTemplate.vue
+++ src/main/resources/templates/ItemDemoUI/src/components/RegisterPageTemplate.vue
@@ -1,3 +1,15 @@
 <template>
-	
+  <div class="regisitertemplate">
+    <div class="heads">
+      页面模板基本信息
+    </div>
+
+  </div>
+
 </template>
+
+<style>
+  .heads{
+    background: lightcyan;
+  }
+</style>
