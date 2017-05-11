5536b789bed4d0338cd75a40a18efc6c61b3c586
wxfei <805915717@qq.com>
2017-05-11 14:46:55 +0800

    uodate tab

--- src/main/resources/templates/ItemDemoUI/src/components/Tab.vue
+++ src/main/resources/templates/ItemDemoUI/src/components/Tab.vue
@@ -1,9 +1,21 @@
 <template>
   <ul class="nav nav-tabs">
-    <li v-bind:class="{'tab-active':isActive==1}" @click="changeTab(1)"><router-link to="/bzprocess" class="link">流程(1)</router-link></li>
-    <li v-bind:class="{'tab-active':isActive==2}" @click="changeTab(2)"><router-link to="/pagetemplate"  class="link" >页面模板(3)</router-link></li>
-    <li v-bind:class="{'tab-active':isActive==3}" @click="changeTab(3)"><router-link to="/bzability" class="link" >业务能力(3)</router-link></li>
-    <li v-bind:class="{'tab-active':isActive==4}" @click="changeTab(4)"><router-link to="/ability" class="link" >能力(3)</router-link></li>
+    <li v-bind:class="{'tab-active':isActive==1}" @click="changeTab(1)">
+      <div class="tab-icon tab-icon-process" v-bind:class="{'tab-icon-process-active':isActive==1}"></div>
+      <router-link to="/bzprocess" class="link">流程(1)</router-link>
+    </li>
+    <li v-bind:class="{'tab-active':isActive==2}" @click="changeTab(2)">
+      <div class="tab-icon tab-icon-temp" v-bind:class="{'tab-icon-temp-active':isActive==2}"></div>
+      <router-link to="/pagetemplate"  class="link" >页面模板(3)</router-link>
+    </li>
+    <li v-bind:class="{'tab-active':isActive==3}" @click="changeTab(3)">
+      <div class="tab-icon tab-icon-bza" v-bind:class="{'tab-icon-bza-active':isActive==3}"></div>
+      <router-link to="/bzability" class="link" >业务能力(3)</router-link>
+    </li>
+    <li v-bind:class="{'tab-active':isActive==4}" @click="changeTab(4)">
+      <div class="tab-icon tab-icon-abl" v-bind:class="{'tab-icon-zbl-active':isActive==4}"></div>
+      <router-link to="/ability" class="link" >能力(3)</router-link>
+    </li>
    </ul>
 </template>
 
