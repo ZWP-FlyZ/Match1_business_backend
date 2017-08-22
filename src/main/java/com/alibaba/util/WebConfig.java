ea684783f1f90c248ac751dc544a2651a11bb694
Tony Yu <602232939@qq.com>
2017-08-22 11:20:45 +0800

    update ignore file

---
+++ src/main/java/com/alibaba/util/WebConfig.java
@@ -0,0 +1,24 @@
+package com.alibaba.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Web MVC config
+ * Created by lucas on 2016/11/11.
+ * Added and edited by tony on 2017/04/22
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true).maxAge(3600);
+    }
+
+}
